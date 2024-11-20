8febbb02ca7a4ea32cb17fd1d37e899ab5529ef4
lelouvincx <dinhminhchinh3357@gmail.com>
2024-11-21 01:12:57 +0700

    Update colorscheme catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,32 @@
 return {
-    -- Installs
-    { "ellisonleao/gruvbox.nvim" },
-    { "folke/tokyonight.nvim" },
-    { "catppuccin/nvim" },
-
+    { "EdenEast/nightfox.nvim" },
     {
-        "LazyVim/LazyVim",
+        "catppuccin/nvim",
+        lazy = true,
+        name = "catppuccin",
         opts = {
-            colorscheme = "catppuccin",
+            flavour = "mocha",
+            background = { -- :h background
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = true,
+            default_integrations = true,
             integrations = {
+                aerial = true,
                 alpha = true,
                 cmp = true,
+                dashboard = true,
                 flash = true,
+                grug_far = true,
                 gitsigns = true,
+                headlines = true,
                 illuminate = true,
                 indent_blankline = { enabled = true },
+                leap = true,
                 lsp_trouble = true,
                 mason = true,
+                markdown = true,
                 mini = true,
                 native_lsp = {
                     enabled = true,
@@ -29,14 +39,33 @@ return {
                 },
                 navic = { enabled = true, custom_bg = "lualine" },
                 neotest = true,
+                neotree = true,
                 noice = true,
                 notify = true,
-                neotree = true,
                 semantic_tokens = true,
                 telescope = true,
                 treesitter = true,
+                treesitter_context = true,
                 which_key = true,
             },
+        },
+        specs = {
+            {
+                "akinsho/bufferline.nvim",
+                optional = true,
+                opts = function(_, opts)
+                    if (vim.g.colors_name or ""):find("catppuccin") then
+                        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+                    end
+                end,
+            },
+        },
+    },
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
         },
     },
 }
