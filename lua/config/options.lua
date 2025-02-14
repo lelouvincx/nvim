d307dd246985164dc1cf7bde4ea90f914047922f
lelouvincx <dinhminhchinh3357@gmail.com>
2025-02-14 21:46:57 +0700

    chore: disable checkhealth for ruby and perl

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,8 +5,8 @@
 vim.g.loaded_python3_provider = 1
 vim.g.loaded_python_provider = 1
 vim.g.loaded_node_provider = 1
-vim.g.loaded_ruby_provider = 1
-vim.g.loaded_perl_provider = 1
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
 
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
