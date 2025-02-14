042b05ee1fc8ba206e102adf1feebf9ec98f3c34
lelouvincx <dinhminhchinh3357@gmail.com>
2025-02-14 21:46:25 +0700

    feat(copilot-chat): add markdown renderer config and misc config

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,4 +1,10 @@
 local telescope = require("CopilotChat.integrations.telescope")
+
+-- Registers copilot-chat filetype for markdown rendering
+require("render-markdown").setup({
+    file_types = { "markdown", "copilot-chat" },
+})
+
 return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
@@ -9,11 +15,17 @@ return {
         return {
             model = "o3-mini",
             agent = "copilot",
+            context = nil, -- Default context or array of contexts to use (can be specified manually in prompt via #).
+            sticky = nil, -- Default sticky prompt or array of sticky prompts to use at start of every new chat.
+
             temperature = 0.1,
+            headless = false, -- Do not write to chat buffer and use history(useful for using callback for custom processing)
+            callback = nil, -- Callback to use when ask response is received
 
             question_header = "  " .. user .. " ", -- Header to use for user questions
             answer_header = "  Copilot ", -- Header to use for AI answers
-            separator = "───", -- Separator to use in chat
+            error_header = "> [!ERROR] Error",
+            separator = "", -- Separator to use in chat
 
             window = {
                 layout = "vertical", -- 'vertical', 'horizontal', 'float', 'replace'
@@ -32,40 +44,40 @@ return {
             show_help = true, -- Shows help message as virtual lines when waiting for user input
             show_folds = true, -- Shows folds for sections in chat
             highlight_selection = true, -- Highlight selection
-            highlight_headers = true, -- Highlight headers in chat, disable if using markdown renderers (like render-markdown.nvim)
-            auto_follow_cursor = true, -- Auto-follow cursor in chat
+            highlight_headers = false, -- Highlight headers in chat, disable if using markdown renderers (like render-markdown.nvim)
+            auto_follow_cursor = false, -- Auto-follow cursor in chat
             auto_insert_mode = false,
             insert_at_end = true, -- Move cursor to end of buffer when inserting text
             clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
 
             -- Custom prompts
             prompts = {
-                Explain = {
-                    prompt = "> /COPILOT_EXPLAIN\n> #buffer\n\nCan you explain the following part of the code in detail?\nSpecifically:\n1. What is the purpose of this section?\n2. How does it work step-by-step?\n3. Are there any potential issues or limitations with this approach?",
+                explain = {
+                    prompt = "> /COPILOT_EXPLAIN\n> #buffer\n\nThink step by step. Can you explain the following part of the code in detail?\nSpecifically:\n1. What is the purpose of this section?\n2. How does it work step-by-step?\n3. Are there any potential issues or limitations with this approach?",
                 },
-                Review = {
-                    prompt = "> /COPILOT_REVIEW\n> #files\n\nPlease review the following code. Consider following criterias:\n1. Code quality and adherence to best practices\n2. Potential bugs or edge cases\n3. Performance optimizations\n4. Readability and maintainability\n5. Any security concerns\nSuggest improvements and explain your reasoning for each suggestion.",
+                review = {
+                    prompt = "> /COPILOT_REVIEW\n> #buffers\n\nPlease review the following code. Consider following criterias:\n1. Code quality and adherence to best practices\n2. Potential bugs or edge cases\n3. Performance optimizations\n4. Readability and maintainability\n5. Any security concerns\nSuggest improvements and explain your reasoning for each suggestion.",
                 },
-                Fix = {
+                fix = {
                     prompt = "> /COPILOT_GENERATE\n> #buffer\n\nThere is a problem in this code. Rewrite the code to show it with the bug fixed.",
                 },
-                Optimize = {
+                optimize = {
                     prompt = "> /COPILOT_GENERATE\n> #buffer\n\nHere's a piece of code that needs optimization. Please suggest optimizations to improve its performance. For each suggestion, explain the expected improvement and any trade-offs.",
                 },
-                OptimizeSQL = {
+                optimizesql = {
                     prompt = "> /COPILOT_GENERATE\n> #buffer\n\nI need to optimize the following SQL query.\nThe query is currently taking too long to execute on large datasets. Please:\n1. Analyze the query and identify potential performance issues.\n2. Suggest optimizations, which may include:\n- Rewriting the query\n- Adding or modifying indexes\n- Suggestions for schema changes if necessary\n3. Explain the reasoning behind each optimization.\n4. If possible, provide an estimate of the performance improvement we might expect.\nAdditional context:\n- Database system: {database_system}\n- Approximate table sizes: {table_size}\n- Any relevant hardware constraints. It is your job to ask me about the database_system and table_size. Is it understood?",
                 },
-                Docstring = {
+                docstring = {
                     prompt = "> /COPILOT_GENERATE\n> #buffer\n\nGenerate appropriate comments and docstrings for the following code.\n\nFollow conventions for docstrings. Include:\n1. Brief description of the function/class\n2. Parameters and their types\n3. Return value and type\n4. Any exceptions that might be raised\n5. Usage examples if the function/class usage is not immediately obvious\n\nPlease use clear, concise language and include relevant code snippets where appropriate.",
                 },
-                Tests = {
+                tests = {
                     prompt = "> /COPILOT_GENERATE\n> #buffer\n\nGenerate unit tests for the following code snippet. Include tests for:\n1. Normal expected inputs\n2. Edge cases\n3. Invalid inputs",
                 },
-                Commit = {
-                    prompt = "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.",
+                commit = {
+                    prompt = "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title does not exceed 50 characters. Wrap the whole message in code block with language gitcommit.",
                 },
-                Implement = {
-                    prompt = "> /COPILOT_GENERATE\n> #buffer\n\nBased on what I have provided, I need to implement a specific functionality in. It's your task to ask me about the functionality, programming language, and key requirements.\n\nPlease consider:\n- Error handling\n- Edge cases\n- Performance optimization\nBest practices\nPlease do not unnecessarily remove any comments or code.\nGenerate the code with clear comments explaining the logic.",
+                implement = {
+                    prompt = "> /COPILOT_GENERATE\n> #buffer\n\nThink step by step. Based on what I have provided, I need to implement a specific functionality in. It's your task to ask me about the functionality, programming language, and key requirements.\n\nPlease consider:\n- Error handling\n- Edge cases\n- Performance optimization\nBest practices\nPlease do not unnecessarily remove any comments or code.\nGenerate the code with clear comments explaining the logic.",
                 },
             },
         }
