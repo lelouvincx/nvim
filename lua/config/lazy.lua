f131966ab91e4bec06573b7809170725d260c3b6
lelouvincx <dinhminhchinh3357@gmail.com>
2025-04-14 09:45:34 +0700

    fix: remove configuration for java

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
         -- extras lang
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
-        { import = "lazyvim.plugins.extras.lang.java" },
         { import = "lazyvim.plugins.extras.lang.docker" },
         { import = "lazyvim.plugins.extras.lang.sql" },
         { import = "lazyvim.plugins.extras.lang.json" },
