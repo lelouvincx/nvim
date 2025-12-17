59e01d52cf5910bbb930558c5c6b52fc36fefac9
lelouvincx <dinhminhchinh3357@gmail.com>
2025-12-17 22:55:41 +0700

    fix(rainbow): upgrade to new github repository

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -1,20 +1,29 @@
+-- lua/plugins/rainbow.lua
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = { "HiPhish/nvim-ts-rainbow2" },
-    opts = function(_, opts)
-        opts.rainbow = {
-            enable = true,
-            disable = {},
-            query = "rainbow-parens",
-            strategy = require("ts-rainbow").strategy.global,
-            hlgroups = {
-                "TSRainbowGreen",
-                "TSRainbowYellow",
-                "TSRainbowRed",
-                "TSRainbowBlue",
-                "TSRainbowViolet",
-                "TSRainbowOrange",
-                "TSRainbowCyan",
+    "HiPhish/rainbow-delimiters.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+        local rainbow_delimiters = require("rainbow-delimiters")
+
+        ---@type rainbow_delimiters.config
+        vim.g.rainbow_delimiters = {
+            strategy = {
+                [""] = rainbow_delimiters.strategy.global,
+            },
+            query = {
+                [""] = "rainbow-delimiters",
+            },
+            priority = {
+                [""] = 110,
+            },
+            highlight = {
+                "RainbowDelimiterGreen",
+                "RainbowDelimiterYellow",
+                "RainbowDelimiterRed",
+                "RainbowDelimiterBlue",
+                "RainbowDelimiterViolet",
+                "RainbowDelimiterOrange",
+                "RainbowDelimiterCyan",
             },
         }
     end,
