5f51de90ac5a41f790db402536f9d44a9cf2a22a
lelouvincx <dinhminhchinh3357@gmail.com>
2025-12-17 23:44:43 +0700

    fix(blink): refactor code to be cleaner

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,8 +1,7 @@
 return {
     "saghen/blink.cmp",
-    version = not vim.g.blink_enabled and "*",
-    event = "InsertEnter",
     enabled = vim.g.blink_enabled,
+    event = "InsertEnter",
     build = vim.g.blink_enabled and "RUSTUP_TOOLCHAIN=nightly cargo build --release",
     opts_extend = {
         "sources.completion.enabled_providers",
@@ -11,177 +10,62 @@ return {
     },
     dependencies = {
         "rafamadriz/friendly-snippets",
-        -- add blink.compat to dependencies
+        "bydlw98/blink-cmp-env",
+        "mikavilpas/blink-ripgrep.nvim",
+        { "L3MON4D3/LuaSnip", version = "v2.*" },
         {
-            "saghen/blink.compat",
-            optional = true, -- make optional so it's only enabled if any extras need it
-            opts = {},
-            version = not vim.g.blink_enabled and "*",
-        },
-        "bydlw98/blink-cmp-env", -- for env completion
-        "mikavilpas/blink-ripgrep.nvim", --for ripgrep completion
-        { "L3MON4D3/LuaSnip", version = "v2.*" }, -- for luasnip completion
-        {
-            "fang2hou/blink-copilot", -- for copilot completion
-            opts = {
-                max_completions = 5,
-                max_attempts = 3,
-            },
+            "fang2hou/blink-copilot",
+            opts = { max_completions = 5, max_attempts = 3 },
         },
     },
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
+        -- Use luasnip as the snippet engine
         snippets = { preset = "luasnip" },
 
+        -- Apperance: LazyVim already handles kind icons, so we just set the variant here
         appearance = {
-            -- sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release, assuming themes add support
-            use_nvim_cmp_as_default = false,
-            -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- adjusts spacing to ensure icons are aligned
             nerd_font_variant = "normal",
-            -- Instead of using default icons, we use the ones from LazyVim
             kind_icons = vim.tbl_extend(
                 "force",
                 require("blink.cmp.config.appearance").kind_icons or {},
                 LazyVim.config.icons.kinds
             ),
-            -- INFO: Default kind_icons
-            -- kind_icons = {
-            --     Text = "󰉿",
-            --     Method = "󰊕",
-            --     Function = "󰊕",
-            --     Constructor = "󰒓",
-            --     Field = "󰜢",
-            --     Variable = "󰆦",
-            --     Property = "󰖷",
-            --     Class = "󱡠",
-            --     Interface = "󱡠",
-            --     Struct = "󱡠",
-            --     Module = "󰅩",
-            --     Unit = "󰪚",
-            --     Value = "󰦨",
-            --     Enum = "󰦨",
-            --     EnumMember = "󰦨",
-            --     Keyword = "󰻾",
-            --     Constant = "󰏿",
-            --     Snippet = "󱄽",
-            --     Color = "󰏘",
-            --     File = "󰈔",
-            --     Reference = "󰬲",
-            --     Folder = "󰉋",
-            --     Event = "󱐋",
-            --     Operator = "󰪚",
-            --     TypeParameter = "󰬛",
-            -- },
         },
 
         completion = {
-            accept = {}, -- use all default https://cmp.saghen.dev/configuration/reference.html#completion-accept
-            menu = {
-                border = "rounded",
-                draw = {
-                    treesitter = { "lsp" },
-                },
-                scrollbar = false, -- dont show the scrollbar
-            },
-            list = {
-                max_items = 50,
-            },
-            documentation = {
-                auto_show = true,
-                window = {
-                    border = "rounded",
-                },
-            },
-            ghost_text = {
-                enabled = true, -- only show for ai completion
-            },
+            menu = { border = "rounded" },
+            documentation = { window = { border = "rounded" }, auto_show = true },
+            ghost_text = { enabled = true },
         },
 
-        -- experimental signature help support
-        signature = {
-            enabled = true,
-            window = { border = "rounded" },
-        },
+        signature = { window = { border = "rounded" }, enabled = true },
 
         sources = {
-            -- adding any nvim-cmp sources here will enable them
-            -- with blink.compat
-            per_filetype = { sql = { "dadbod" } },
-            default = { "copilot", "omni", "buffer", "snippets", "lsp", "path", "ripgrep" },
+            default = { "copilot", "lsp", "path", "snippets", "buffer", "ripgrep" },
+            per_filetype = { sql = { "dadbod", "buffer" } },
             providers = {
-                --[[
-                INFO: score_offset order list (still keep for referencing purpose):
-                - copilot: 10
-                - omni: 6
-                - path: 3
-                - lsp: 0
-                - buffer: -3
-                - snippets: -1
-                - lazydev: -2
-                - ripgrep: -4
-                - dadbod: -5
-                - env: -6
-                ]]
                 copilot = {
-                    name = "Copilot",
+                    name = "copilot",
                     module = "blink-copilot",
                     score_offset = 10,
                     async = true,
-                },
-                omni = {
-                    score_offset = 6,
-                },
-                lazydev = {
-                    name = "LazyDev",
-                    module = "lazydev.integrations.blink",
-                    score_offset = -2,
+                    kind = "Copilot", -- Defined in transform_items if needed
                 },
                 ripgrep = {
                     name = "Ripgrep",
                     module = "blink-ripgrep",
                     score_offset = -4,
-                    -- options below are optional, these are the default values
-                    opts = {
-                        -- `min_keyword_length` only determines whether to show completion items in the menu,
-                        -- not whether to trigger a search. And we only has one chance to search.
-                        prefix_min_len = 3,
-                        get_command = function(context, prefix)
-                            return {
-                                "rg",
-                                "--no-config",
-                                "--json",
-                                "--word-regexp",
-                                "--ignore-case",
-                                "--",
-                                prefix .. "[\\w_-]+",
-                                vim.fs.root(0, ".git") or vim.fn.getcwd(),
-                            }
-                        end,
-                        get_prefix = function(context)
-                            return context.line:sub(1, context.cursor[2]):match("[%w_-]+$") or ""
-                        end,
-                    },
-                },
-                dadbod = {
-                    kind = "DadBod",
-                    module = "vim_dadbod_completion.blink",
-                    score_offset = -5,
                 },
                 env = {
                     name = "Env",
                     module = "blink-cmp-env",
                     score_offset = -6,
-                    --- @type blink-cmp-env.Options
-                    opts = {
-                        item_kind = require("blink.cmp.types").CompletionItemKind.Variable,
-                        show_braces = false,
-                        show_documentation_window = true,
-                    },
+                },
+                dadbod = {
+                    name = "DadBod",
+                    module = "vim_dadbod_completion.blink",
+                    score_offset = -5,
                 },
             },
         },
