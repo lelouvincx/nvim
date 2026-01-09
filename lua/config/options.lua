017fd07883a42d5aebbc21d48194e8f4817e1495
lelouvincx <dinhminhchinh3357@gmail.com>
2026-01-09 17:40:16 +0700

    fix(options): unload languages: python, python3, node

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,9 +7,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Disable unused language providers to speed up startup time
-vim.g.loaded_python3_provider = 1
-vim.g.loaded_python_provider = 1
-vim.g.loaded_node_provider = 1
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 
