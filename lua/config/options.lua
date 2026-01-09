812cc67f23198d50f783d05742eda3eac47ea5d4
lelouvincx <dinhminhchinh3357@gmail.com>
2026-01-09 17:49:23 +0700

    refactor(ftdetect): migrate into options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -43,3 +43,21 @@ vim.g.lazyvim_picker = "telescope"
 
 -- Expand claude config dir
 vim.env.CLAUDE_CONFIG_DIR = vim.fn.expand("~/.config/claude")
+
+-- ftdetect settings
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.dbml",
+    command = "setfiletype dbml",
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.env.*",
+    command = "setfiletype sh",
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.envrc.*",
+    command = "setfiletype sh",
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = ".zimrc",
+    command = "setfiletype sh",
+})
