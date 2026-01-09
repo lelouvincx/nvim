5c574ee2297a204ecc619fc0bcdc7a03534ada57
lelouvincx <dinhminhchinh3357@gmail.com>
2026-01-09 17:39:09 +0700

    refactor(lspconfig): improve file structure

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,35 +1,30 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "SmiteshP/nvim-navbuddy", -- Provides breakcrumbs for better navigation
-        dependencies = {
-            "SmiteshP/nvim-navic",
-            "MunifTanjim/nui.nvim",
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            -- LazyVim merges this into its lspconfig setup
+            diagnostics = {
+                virtual_text = { prefix = "■ " },
+                float = { border = "rounded", focusable = true },
+            },
+            -- Apply to all servers via LazyVim's setup
+            servers = {
+                lua_ls = {},
+                pyright = {},
+                ruff = {},
+            },
         },
+        init = function()
+            -- Global LSP UI customizations
+            local border = "rounded"
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
+            vim.lsp.handlers["textDocument/signatureHelp"] =
+                vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
+        end,
+    },
+    {
+        "SmiteshP/nvim-navbuddy",
+        dependencies = { "SmiteshP/nvim-navic", "MunifTanjim/nui.nvim" },
         opts = { lsp = { auto_attach = true } },
     },
-    opts = function()
-        -- Add the border on hover and on signature help popup window
-        local handlers = {
-            ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-            ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-        }
-
-        -- Add border to the diagnostic popup window
-        vim.diagnostic.config({
-            virtual_text = {
-                prefix = "■ ", -- Could be '●', '▎', 'x', '■', , 
-            },
-            float = { border = "rounded", focusable = true },
-        })
-
-        local lspconfig = require("lspconfig")
-        -- INFO: Add the handlers to all the servers
-        -- Need to add more when needed
-        local servers = { "lua_ls", "pyright", "ruff" }
-
-        for _, lsp in ipairs(servers) do
-            lspconfig[lsp].setup({ handlers = handlers })
-        end
-    end,
 }
