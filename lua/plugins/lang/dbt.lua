dced39e9823e3688e64ff514b993951ce2f3e50e
lelouvincx <dinhminhchinh3357@gmail.com>
2026-01-13 08:55:31 +0700

    ci(release): change release schedule to every 2 months

--- lua/plugins/lang/dbt.lua
+++ lua/plugins/lang/dbt.lua
@@ -34,4 +34,40 @@ return {
             vim.list_extend(opts.ensure_installed, { "sql", "jinja", "jinja_inline" })
         end,
     },
+
+    {
+        "PedramNavid/dbtpal",
+        lazy = true,
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+        ft = { "sql", "yaml", "md" },
+        keys = {
+            { "<leader>mr", "<cmd>DbtRun<cr>", desc = "Run Current Model" },
+            { "<leader>mt", "<cmd>DbtTest<cr>", desc = "Test Current Model" },
+            { "<leader>ma", "<cmd>lua require('dbtpal').run_all()<cr>", desc = "Run All Models" },
+            { "<leader>md", "<cmd>lua require('dbtpal.telescope').dbt_picker()<cr>" },
+        },
+        config = function()
+            require("dbtpal").setup({
+                path_to_dbt = "dbt",
+                path_to_dbt_project = "",
+                path_to_dbt_profiles_dir = vim.fn.expand("~/.dbt"),
+                extended_path_search = true,
+                protect_compiled_files = true,
+            })
+            require("telescope").load_extension("dbtpal")
+        end,
+    },
+
+    {
+        "folke/which-key.nvim",
+        opts = {
+            spec = {
+                {
+                    "<leader>m",
+                    group = "dbt",
+                    icon = "🛢️",
+                },
+            },
+        },
+    },
 }
